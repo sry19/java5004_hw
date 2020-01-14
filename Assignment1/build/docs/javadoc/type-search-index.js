1c73a9404875586ce751fc60e0a4d12e82389bb6
rebeccasun <sun.ruoy@husky.neu.edu>
2020-01-13 23:29:47 -0800

    add gitignore

--- Assignment1/build/docs/javadoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"p":"problem3","l":"Account"},{"l":"All Classes","url":"allclasses-index.html"},{"p":"problem3","l":"Amount"},{"p":"problem3","l":"Holder"},{"p":"problem1","l":"Owner"},{"p":"problem2","l":"Runner"},{"p":"problem2","l":"Time"},{"p":"problem1","l":"Vehicle"}]
--- Lab1/build/docs/javadoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"p":"<Unnamed>","l":"Address"},{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Author"},{"p":"<Unnamed>","l":"Email"},{"p":"<Unnamed>","l":"Person"}]
