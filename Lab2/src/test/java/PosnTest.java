236b14bd397cfa1619cc6724e597c4f206fcaf1b
rebeccasun <sun.ruoy@husky.neu.edu>
2020-01-15 22:06:43 -0800

    lab2 p2

--- Lab2/src/test/java/PosnTest.java
+++ Lab2/src/test/java/PosnTest.java
@@ -9,6 +9,7 @@ public class PosnTest {
   Posn posn2;
   Posn posn3;
   Posn posn4;
+  Name name;
   private Integer CORRECT_X = 3;
   private Integer WRONG_X = 4;
   private Integer CORRECT_Y = 4;
@@ -20,28 +21,37 @@ public class PosnTest {
     posn2 = new Posn(CORRECT_X,CORRECT_Y);
     posn3 = new Posn(WRONG_X,CORRECT_Y);
     posn4 = new Posn(CORRECT_X,WRONG_Y );
+    name = new Name("Amy","n/a","Brown");
   }
 
   @Test
   public void getX() {
-    assertEquals((Integer)3,posn1.getX());
+    assertEquals(CORRECT_X,posn1.getX());
   }
 
   @Test
   public void getY() {
-    assertEquals((Integer)4,posn1.getY());
+    assertEquals(CORRECT_Y,posn1.getY());
   }
 
   @Test
   public void testEquals() {
-
+    assertTrue(posn1.equals(posn2));
+    assertFalse(posn1.equals(null));
+    assertFalse(posn1.equals(posn3));
+    assertFalse(posn1.equals(posn4));
+    assertFalse(posn1.equals(name));
   }
 
   @Test
   public void testHashCode() {
+    assertEquals(posn1.hashCode(),posn2.hashCode());
+    assertNotEquals(posn1.hashCode(),posn3.hashCode());
+    assertNotEquals(posn1.hashCode(),posn4.hashCode());
   }
 
   @Test
   public void testToString() {
+    assertEquals("Posn",posn1.toString());
   }
 }
