79244f692aae15fbd3a045dbfd0b49a70d65c98e
rebeccasun <sun.ruoy@husky.neu.edu>
2020-02-07 22:22:58 -0800

    javadoc

--- Assignment5/src/main/java/problem2/Creators.java
+++ Assignment5/src/main/java/problem2/Creators.java
@@ -4,5 +4,6 @@ package problem2;
  * The type Creators.
  */
 public abstract class Creators implements ICreator {
+
   public abstract Boolean match(Creators creator);
 }
--- Assignment5/src/main/java/problem2/ICreator.java
+++ Assignment5/src/main/java/problem2/ICreator.java
@@ -4,5 +4,12 @@ package problem2;
  * The interface Creator.
  */
 public interface ICreator {
+
+  /**
+   * Match boolean.
+   *
+   * @param creator the creator
+   * @return the boolean
+   */
   public Boolean match(Creators creator);
 }
