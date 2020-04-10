3e49a8c2795496cd8140e13fd4ae154339e48513
rebeccasun <sun.ruoy@husky.neu.edu>
2020-04-10 15:42:35 -0700

    finish basic struction

---
+++ hw10/src/main/java/reservationsystem/model/Row.java
@@ -0,0 +1,97 @@
+package reservationsystem.model;
+
+import java.util.ArrayList;
+
+import java.util.List;
+
+
+public class Row extends ArrayList<Seat> {
+
+  private Integer rowNumber;
+  private boolean wheelchairAccessible;
+  private Integer numOfSeats;
+  private static final int MAX_SEATS = 16;
+
+  public Row(Integer rowNumber, boolean wheelchairAccessible) {
+    super(Row.createRow());
+    this.rowNumber = rowNumber;
+    this.wheelchairAccessible = wheelchairAccessible;
+    this.numOfSeats = MAX_SEATS;
+  }
+
+  public static List<Seat> createRow() {
+    List<Seat> seats = new ArrayList<>();
+    for (int i = 0;i<MAX_SEATS;i++) {
+      seats.add(new Seat((char) (65 + i),null));
+    }
+    return seats;
+  }
+
+  public Integer getRowNumber() {
+    return rowNumber;
+  }
+
+  public boolean isWheelchairAccessible() {
+    return wheelchairAccessible;
+  }
+
+  public Integer getNumOfSeats() {
+    return numOfSeats;
+  }
+
+  public void setNumOfSeats(Integer numOfSeats) {
+    this.numOfSeats = numOfSeats;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    if (!super.equals(o)) {
+      return false;
+    }
+
+    Row row = (Row) o;
+
+    if (wheelchairAccessible != row.wheelchairAccessible) {
+      return false;
+    }
+    if (!rowNumber.equals(row.rowNumber)) {
+      return false;
+    }
+    return numOfSeats.equals(row.numOfSeats);
+  }
+
+  public void setWheelchairAccessible(boolean wheelchairAccessible) {
+    this.wheelchairAccessible = wheelchairAccessible;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = super.hashCode();
+    result = 31 * result + rowNumber.hashCode();
+    result = 31 * result + (wheelchairAccessible ? 1 : 0);
+    result = 31 * result + numOfSeats.hashCode();
+    return result;
+  }
+
+  @Override
+  public String toString() {
+    String s = String.format(" %2d ",this.rowNumber);
+    for (Seat seat: this) {
+      s += this.checkToString(seat.toString());
+    }
+    return s;
+  }
+
+  private String checkToString(String original) {
+    if (original.equals(" _") && this.wheelchairAccessible) {
+      return " =";
+    }
+    return original;
+  }
+}
---
+++ hw10/src/test/java/reservationsystem/model/RowTest.java
@@ -0,0 +1,49 @@
+package reservationsystem.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class RowTest {
+
+  @Before
+  public void setUp() throws Exception {
+  }
+
+  @Test
+  public void createRow() {
+  }
+
+  @Test
+  public void getRowNumber() {
+  }
+
+  @Test
+  public void isWheelchairAccessible() {
+  }
+
+  @Test
+  public void getNumOfSeats() {
+  }
+
+  @Test
+  public void setNumOfSeats() {
+  }
+
+  @Test
+  public void testEquals() {
+  }
+
+  @Test
+  public void setWheelchairAccessible() {
+  }
+
+  @Test
+  public void testHashCode() {
+  }
+
+  @Test
+  public void testToString() {
+  }
+}
