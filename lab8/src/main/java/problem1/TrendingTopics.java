74900626306ea3f16e4748e0d8f0d1f7bdf99ccb
rebeccasun <sun.ruoy@husky.neu.edu>
2020-04-15 13:15:03 -0700

    javadoc

--- lab8/src/main/java/problem1/TrendingTopics.java
+++ lab8/src/main/java/problem1/TrendingTopics.java
@@ -5,7 +5,17 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+/**
+ * The type Trending topics.
+ */
 public class TrendingTopics {
+
+  /**
+   * Count topics map.
+   *
+   * @param input the input
+   * @return the map
+   */
   public static Map<String,Long> countTopics(List<String> input) {
     Map<String,Long> result = input.stream().collect(Collectors.groupingBy(e -> e, Collectors.counting()));
     return result;
